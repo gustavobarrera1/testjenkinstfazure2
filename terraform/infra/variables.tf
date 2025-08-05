388b4c415806b12e9033aa4b80c21170094a3fed
Gustavo Barrera <gustavobarrera1@outlook.es>
2025-08-05 13:14:56 -0300

    test new subscription

--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -17,14 +17,14 @@ variable "containerapp_name" {
 ##
 
 variable "client_id" {
-  default        = "b24fa600-ff3d-414a-9802-79047c858bce"
+  #default        = "b24fa600-ff3d-414a-9802-79047c858bce"
 }
 variable "client_secret" {
   type        = string
 }
 variable "tenant_id" {
-  default        = "8ee9d595-4f94-41e5-a20c-b29b4e64578b"
+  #default        = "8ee9d595-4f94-41e5-a20c-b29b4e64578b"
 }
 variable "subscription_id" {
-  default = "4dc63939-80f6-4f50-bd19-bc605cf2786d"
+  #default = "4dc63939-80f6-4f50-bd19-bc605cf2786d"
 }
