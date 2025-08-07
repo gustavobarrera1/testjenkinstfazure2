78474043f7389f9d15ef4b30eb3659c6a9f2c388
Gustavo Barrera <gustavobarrera1@outlook.es>
2025-08-07 20:20:56 -0300

    test new subscription 7

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -1,19 +1,12 @@
-# variable "subscription_id" {
-#   description = "Azure Subscription ID"
-#   type        = string
-# }
-
-##
-
 variable "client_id" {
-  #default        = "b24fa600-ff3d-414a-9802-79047c858bce"
+  default        = "39a8af82-58e1-4533-ac23-0a0022c8e745"
 }
 variable "client_secret" {
   type        = string
 }
 variable "tenant_id" {
-  #default        = "8ee9d595-4f94-41e5-a20c-b29b4e64578b"
+  default        = "68e258f3-5801-48ce-9128-520f28ecc0a4"
 }
 variable "subscription_id" {
-  #default = "4dc63939-80f6-4f50-bd19-bc605cf2786d"
+  default = "9734eafc-3f58-4aa1-9930-1d6a4a9c500c"
 }
